e9c0c34548e52da14d48c7710460162efc70526a
Pranali <pranalisonawane050@gmail.com>
2021-09-24 14:22:19 +0530

    added file

--- src/FileNotFoundExceptionExample.java
+++ src/FileNotFoundExceptionExample.java
@@ -2,26 +2,14 @@ import java.io.*;
 import java.io.FileNotFoundException;
 public class FileNotFoundExceptionExample
 {
-    public void checkFileNotFound()
-    {
-    	
-    //	System.out.println(new File("input.txt").getAbsolutePath());
-        try
-        {            
-        	FileInputStream in = new FileInputStream("input.txt");
-            System.out.println("This is not printed");
-           
-        } 
-        catch (IOException e)
-        {
-        	
-            e.printStackTrace();
+    	public static void main(String args[])  {
+            try {
+    
+                File file = new File("E://file.txt");
+                FileReader fr = new FileReader(file);
+            } catch (FileNotFoundException e) {
+               System.out.println("File does not exist");
+               System.out.println(e);
+            }
         }
-        
-    }
-    public static void main(String[] args)
-    {
-        FileNotFoundExceptionExample example = new FileNotFoundExceptionExample();
-        example.checkFileNotFound();
-    }
 }
